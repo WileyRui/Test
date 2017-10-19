881916846b2f5649cc4411bc205745a2131a11fc
huanglei <13732273692@163.com>
2017-10-19 10:03:50 +0800

    排除无效日期

--- src/main/java/com/apin/airline/common/mapper/QueryMapper.java
+++ src/main/java/com/apin/airline/common/mapper/QueryMapper.java
@@ -21,7 +21,7 @@ public interface QueryMapper extends Mapper {
     @Select("select img_url from msd_city where city_name=#{deatCity}")
     String selectCityImg(String destCity);
     @Select("select b.flight_date retDate,min(b.adult_price) basePrice,sum(b.seat_count) remainCount from mbs_airline a join mbs_airline_flight b on a.id=b.airline_id" +
-            " join msd_airline c on a.airline_id=c.id where c.dep_city=#{depCity} and c.arr_city=#{arrCity}  and (#{flightType} is null or c.flight_type=#{flightType}) and SUBSTRING_INDEX(b.flight_date,'-',2)=#{month}  and   a.res_type=0 and a.airline_status=1 and a.is_invalid=0   group by b.flight_date order  by b.flight_date")
+            " join msd_airline c on a.airline_id=c.id where c.dep_city=#{depCity} and c.arr_city=#{arrCity}  and (#{flightType} is null or c.flight_type=#{flightType}) and SUBSTRING_INDEX(b.flight_date,'-',2)=#{month} and b.flight_date>=NOW()  and   a.res_type=0 and a.airline_status=1 and a.is_invalid=0   group by b.flight_date order  by b.flight_date")
     List<DayPrice> selectFlightDates(CityList cityList);
     @SelectProvider(type = AspectSql.class,method = "selectFlightList")
     List<ResponseAirlineDto> selectFlightList(CityList cityList);
