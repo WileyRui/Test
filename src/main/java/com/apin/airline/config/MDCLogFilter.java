8f6b39930e68b6b867e2e68a2cf3c8951f32a8e6
赵嵬 <kevinzhao221@163.com>
2017-10-27 12:07:33 +0800

    日志

---
+++ src/main/java/com/apin/airline/config/MDCLogFilter.java
@@ -0,0 +1,32 @@
+package com.apin.airline.config;
+
+import com.apinji.utils.log.MDCInsertingServletFilter;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Created by zhaowei on 2017/10/26.
+ */
+@Component
+@WebFilter(urlPatterns = {"/*"})
+public class MDCLogFilter implements Filter {
+    MDCInsertingServletFilter filter = new MDCInsertingServletFilter();
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+     filter.doFilter(servletRequest,servletResponse,filterChain);
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
